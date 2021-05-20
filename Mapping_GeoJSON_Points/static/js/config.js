b64847f2227456dd43e67b7ad6ed42d92bc9d369
Vinay Mamidala <mamidala.vk@gmail.com>
2021-05-19 21:52:51 -0400

    adding updated files

---
+++ Mapping_GeoJSON_Points/static/js/config.js
@@ -0,0 +1,2 @@
+//API Key
+const API_KEY = "pk.eyJ1IjoibWFtaWRhbGEiLCJhIjoiY2tvdGR6M2hjMDkyazJ3azdrdHZ2NHhlaiJ9.vloHefwBXcSAnHgCqk9EoQ";
--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -29,7 +29,7 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city){
     console.log(city)
-    L.CircleMarker(city.location,{
+    L.circleMarker(city.location,{
         radius: city.population/100000
     })
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3> Population " + city.population.toLocaleString() + "</h3>")
